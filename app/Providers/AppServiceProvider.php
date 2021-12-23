ade6bd01a53fa5d87e40363c69a5b7452467d107
Ajonye Paul <ajonyechase@gmail.com>
2021-12-23 23:59:56 +0100

    fix: migration error of string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
+        Schema::defaultStringLength(191);
     }
 }
